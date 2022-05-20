14d2e950f53362873f87a772a178bff6cc6083a7
Jay Yasay <paolo_yasay@yahoo.com>
2022-05-20 17:36:24 +0400

    Added valorant colour palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'valorant-red-01': '#fa4454',
+        'valorant-red-02': '#dc334b',
+        'valorant-green-01': '#042e27',
+        'valorant-peach-01': '#b78460',
+        'valorant-pink-01': '#b38c8f',
+        'valorant-purple-01': '#364966'
+      },
+    },
   },
   plugins: [],
 }
